0ba78d1bd006a67cfc209c73301ab43d1af40841
yiannnn <92844800+yiannnn@users.noreply.github.com>
2024-10-16 22:30:17 -0700

    Update stock_price.py

--- stock_price.py
+++ stock_price.py
@@ -1,4 +1,4 @@
-# In Cloud Composer, add apache-airflow-providers-snowflake to PYPI Packages
+# In Cloud Composer, add snowflake-connector-python to PYPI Packages
 from airflow import DAG
 from airflow.models import Variable
 from airflow.decorators import task
@@ -12,69 +12,88 @@ import requests
 
 def return_snowflake_conn():
 
-    # Initialize the SnowflakeHook
     hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
     
     # Execute the query and fetch results
     conn = hook.get_conn()
     return conn.cursor()
 
-@task
-def return_last_90d_price(symbol):
-  vantage_api_key = Variable.get('alpha_password')
-  url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={vantage_api_key}'
-  r = requests.get(url)
-  data = r.json()
-  results = []
-  for d in data["Time Series (Daily)"]: # here d is a date: "YYYY-MM-DD"
-    stock_info = data["Time Series (Daily)"][d] 
-    stock_info["date"] = d 
-    results.append(stock_info)
-  return results
 
 @task
-def load_v2(con, records):
-  target_table = "dev.raw_data.stock_price" 
-  try:
-    con.execute("BEGIN;")
-    con.execute(f"""
-    CREATE TABLE IF NOT EXISTS {target_table} (
-      date NUMBER(6,0) PRIMARY KEY, open NUMBER(38,0),
-      high NUMBER(38,0),
-      low NUMBER(38,0),
-      close NUMBER(38,0),
-      volume NUMBER(38,0) 
-    )""")
-    for r in records:
-      open = r["1. open"]
-      high = r["2. high"]
-      low = r["3. low"]
-      close = r["4. close"]
-      volume = r["5. volume"]
-      date = r["date"]
-      insert_sql = f"INSERT INTO {target_table} (date, open, high, low, close, volume) VALUES ({date}, {open}, {high}, {low}, {close}, {volume})"
-      con.execute(insert_sql) 
-    con.execute("COMMIT;")
-  except Exception as e: 
-    con.execute("ROLLBACK;") 
-    print(e)
-    raise e
+def train(cur, train_input_table, train_view, forecast_function_name):
+    """
+     - Create a view with training related columns
+     - Create a model with the view above
+    """
+
+    create_view_sql = f"""CREATE OR REPLACE VIEW {train_view} AS SELECT
+        DATE, CLOSE, STOCK
+        FROM {train_input_table};"""
+
+    create_model_sql = f"""CREATE OR REPLACE SNOWFLAKE.ML.FORECAST {forecast_function_name} (
+        INPUT_DATA => SYSTEM$REFERENCE('VIEW', '{train_view}'),
+        SERIES_COLNAME => 'STOCK',
+        TIMESTAMP_COLNAME => 'DATE',
+        TARGET_COLNAME => 'CLOSE',
+        CONFIG_OBJECT => {{ 'ON_ERROR': 'SKIP' }}
+    );"""
+
+    try:
+        cur.execute(create_view_sql)
+        cur.execute(create_model_sql)
+        # Inspect the accuracy metrics of your model. 
+        cur.execute(f"CALL {forecast_function_name}!SHOW_EVALUATION_METRICS();")
+    except Exception as e:
+        print(e)
+        raise
+
+
+@task
+def predict(cur, forecast_function_name, train_input_table, forecast_table, final_table):
+    """
+     - Generate predictions and store the results to a table named forecast_table.
+     - Union your predictions with your historical data, then create the final table
+    """
+    make_prediction_sql = f"""BEGIN
+        -- This is the step that creates your predictions.
+        CALL {forecast_function_name}!FORECAST(
+            FORECASTING_PERIODS => 7,
+            -- Here we set your prediction interval.
+            CONFIG_OBJECT => {{'prediction_interval': 0.95}}
+        );
+        -- These steps store your predictions to a table.
+        LET x := SQLID;
+        CREATE OR REPLACE TABLE {forecast_table} AS SELECT * FROM TABLE(RESULT_SCAN(:x));
+    END;"""
+    create_final_table_sql = f"""CREATE OR REPLACE TABLE {final_table} AS
+        SELECT STOCK, DATE, CLOSE AS actual, NULL AS forecast, NULL AS lower_bound, NULL AS upper_bound
+        FROM {train_input_table}
+        UNION ALL
+        SELECT replace(series, '"', '') as STOCK, ts as DATE, NULL AS actual, forecast, lower_bound, upper_bound
+        FROM {forecast_table};"""
+
+    try:
+        cur.execute(make_prediction_sql)
+        cur.execute(create_final_table_sql)
+    except Exception as e:
+        print(e)
+        raise
 
 
 with DAG(
-    dag_id = 'stock_v2',
+    dag_id = 'TrainPredict',
     start_date = datetime(2024,9,21),
     catchup=False,
-    tags=['ETL'],
-    schedule = '30 2 * * *'
+    tags=['ML', 'ELT'],
+    schedule = '30 6 * * *'
 ) as dag:
-    #target_table = "dev.raw_data.country_capital"
-    #url = Variable.get("country_capital_url")
+    
+    train_input_table = "dev.raw_data.market_data" #the table you use to store the stock data
+    train_view = "dev.adhoc.market_data_view"
+    forecast_table = "dev.adhoc.market_data_forecast"
+    forecast_function_name = "dev.analytics.predict_stock_price"
+    final_table = "dev.analytics.market_data"
     cur = return_snowflake_conn()
-    symbol = Variable.get("stock_symbol")
-    price_list = return_last_90d_price(symbol)
-    #create_table(cur)
 
-    # data = extract(url)
-    # lines = transform(data)
-    load_v2(cur, price_list)
+    train(cur, train_input_table, train_view, forecast_function_name)
+    predict(cur, forecast_function_name, train_input_table, forecast_table, final_table)
